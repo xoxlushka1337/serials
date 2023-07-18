1a91e4f583afb53dff8750561c867cd21332f963
xoxlushka1337 <xoxlushka@gmail.com>
2023-07-18 13:17:11 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./scss/app.css";
+import "./App.css";
+import "./scss/normalize.scss";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>fghjgjkl;'lkhjg</h1>
+			</header>
+		</div>
+	);
+}
+
+export default App;
