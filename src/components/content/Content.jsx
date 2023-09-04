fe50db6f4455f4b0b07ca4d770c7255dc6b78c36
xoxlushka1337 <xoxlushka@gmail.com>
2023-09-04 13:15:47 +0300

    feat: add background darkening

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -7,12 +7,18 @@ import { ReactComponent as Arrow } from "../../img/icon/arrow.svg";
 const Content = ({ prevMovie, nextMovie, movieIndex }) => {
 	const movie = useSelector(state => state.movies.movies);
 
+	let number = movie[movieIndex].id + 1;
+	if (number <= 9) {
+		number = `0${number}`;
+	}
+
 	return (
 		<div className="content">
 			<div className="content__main">
 				<h1 className="content__title">{movie[movieIndex].title}</h1>
 				<p className="content__description">{movie[movieIndex].description}</p>
 			</div>
+
 			<div className="content__wrapper">
 				<button className={"content__button"} onClick={() => prevMovie()}>
 					<Arrow className="content__arrow" />
@@ -20,8 +26,7 @@ const Content = ({ prevMovie, nextMovie, movieIndex }) => {
 				<button className="content__button" onClick={() => nextMovie()}>
 					<Arrow className="content__arrow content__arrow_turn" />
 				</button>
-
-				<div className="content__number">01</div>
+				<div className="content__number">{number}</div>
 			</div>
 		</div>
 	);
